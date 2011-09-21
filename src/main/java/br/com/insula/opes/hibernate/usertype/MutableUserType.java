326f26f24e681638cc956692af521eb0254e2961
Edson Yanaga <yanaga@insula.com.br>
2011-09-21 11:33:50 -0300

    Criada classe MutableUserType.

--- src/main/java/br/com/insula/opes/hibernate/usertype/MutableUserType.java
+++ src/main/java/br/com/insula/opes/hibernate/usertype/MutableUserType.java
@@ -21,24 +21,9 @@ import java.io.Serializable;
 import org.hibernate.HibernateException;
 import org.hibernate.usertype.UserType;
 
-public abstract class ImmutableUserType implements UserType, Serializable {
+public abstract class MutableUserType implements UserType, Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	@Override
-	public Object assemble(Serializable cached, Object owner) throws HibernateException {
-		return deepCopy(cached);
-	}
-
-	@Override
-	public Object deepCopy(Object value) throws HibernateException {
-		return value;
-	}
-
-	@Override
-	public Serializable disassemble(Object value) throws HibernateException {
-		return (Serializable) deepCopy(value);
-	}
 
 	@Override
 	public boolean equals(Object x, Object y) throws HibernateException {
@@ -58,7 +43,17 @@ public abstract class ImmutableUserType implements UserType, Serializable {
 
 	@Override
 	public boolean isMutable() {
-		return false;
+		return true;
+	}
+
+	@Override
+	public Serializable disassemble(Object value) throws HibernateException {
+		return (Serializable) deepCopy(value);
+	}
+
+	@Override
+	public Object assemble(Serializable cached, Object owner) throws HibernateException {
+		return deepCopy(cached);
 	}
 
 	@Override
